a1ab946f2d3639c42cd2f922a461be0d2de19a02
Damir <damirchelny25@yandex.ru>
2021-09-19 16:51:56 +0300

    Добавлены утилитарные функции

---
+++ js/main.js
@@ -0,0 +1,13 @@
+'use strict';
+const getRandomInt = (min, max) => {
+  if (min < 0 || max < 0 || min > max || min === max) {
+    return -1 ;
+  }
+  return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
+};
+
+
+const checkString =  (text, textLength) => {
+  return text.length <= textLength;
+};
+
