69d51d6f9234ab052e7f4d75564f3f69fccbd3be
Damir <damirchelny25@yandex.ru>
2021-09-30 22:44:36 +0300

    не работает рендеринг

--- js/preview.js
+++ js/preview.js
@@ -6,7 +6,7 @@ const postsContainer = document.querySelector('.pictures');
 
 const addItem = (datum) => {
   const userPost = userPostTemp.cloneNode(true);
-  userPost.querySelector('.picture_img').src = photos[1].url;
+  userPost.querySelector('.picture_img').src = datum.url;
   userPost.querySelector('.picture__comments').textContent = datum.comments.length;
   userPost.querySelector('.picture__likes').textContent = datum.likes;
   return userPost;
